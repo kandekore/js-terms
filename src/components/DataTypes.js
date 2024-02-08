196270b6419e18de2f81747d39c7e418094f0e17
Darren <you@example.comdarren@kandekore.net>
2024-02-08 18:16:45 +0000

    Array & Data Types

--- src/components/DataTypes.js
+++ src/components/DataTypes.js
@@ -2,8 +2,26 @@ import React from "react";
 import Accordion from "react-bootstrap/Accordion";
 import "bootstrap/dist/css/bootstrap.min.css";
 import styles from "./Terms.module.css";
+import { useQuery, gql } from "@apollo/client";
+
+// GraphQL query to fetch variables concepts
+const GET_DATA_TYPE_CONCEPTS = gql`
+  query GetConceptsByCategory {
+    getConceptsByCategory(category: "Data Types") {
+      term
+      description
+      code
+    }
+  }
+`;
 
 function DataTypesPage() {
+  const { loading, error, data } = useQuery(GET_DATA_TYPE_CONCEPTS);
+
+  console.log("data" + data); // Add this line for debugging
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
   return (
     <div>
       <h2>Data Types</h2>
@@ -13,84 +31,17 @@ function DataTypesPage() {
       </p>
 
       <Accordion defaultActiveKey="0" className="mb-3">
-        <Accordion.Item eventKey="0">
-          <Accordion.Header>String</Accordion.Header>
-          <Accordion.Body>
-            <p>Represents textual data.</p>
-            <pre className={styles.codeSnippet}>
-              <code>let greeting = "Hello, world!";</code>
-            </pre>
-          </Accordion.Body>
-        </Accordion.Item>
-
-        <Accordion.Item eventKey="1">
-          <Accordion.Header>Number</Accordion.Header>
-          <Accordion.Body>
-            <p>
-              Represents numerical data, both integers and floating-point
-              numbers.
-            </p>
-            <pre className={styles.codeSnippet}>
-              <code>let age = 30;</code>
-            </pre>
-          </Accordion.Body>
-        </Accordion.Item>
-
-        <Accordion.Item eventKey="2">
-          <Accordion.Header>Boolean</Accordion.Header>
-          <Accordion.Body>
-            <p>
-              Represents a logical entity and can have two values: true and
-              false.
-            </p>
-            <pre className={styles.codeSnippet}>
-              <code>let isAdult = true;</code>
-            </pre>
-          </Accordion.Body>
-        </Accordion.Item>
-
-        <Accordion.Item eventKey="3">
-          <Accordion.Header>Object</Accordion.Header>
-          <Accordion.Body>
-            <p>
-              Represents instances of keyed collections and more complex
-              entities.
-            </p>
-            <pre className={styles.codeSnippet}>
-              <code>{`const person = {\n  firstName: "John",\n  lastName: "Doe",\n  age: 30\n};`}</code>
-            </pre>
-          </Accordion.Body>
-        </Accordion.Item>
-
-        <Accordion.Item eventKey="4">
-          <Accordion.Header>Array</Accordion.Header>
-          <Accordion.Body>
-            <p>Represents a list-like collection of items.</p>
-            <pre className={styles.codeSnippet}>
-              <code>{`let numbers = [1, 2, 3, 4, 5];`}</code>
-            </pre>
-          </Accordion.Body>
-        </Accordion.Item>
-
-        <Accordion.Item eventKey="5">
-          <Accordion.Header>Null</Accordion.Header>
-          <Accordion.Body>
-            <p>Represents the intentional absence of any object value.</p>
-            <pre className={styles.codeSnippet}>
-              <code>{`let empty = null;`}</code>
-            </pre>
-          </Accordion.Body>
-        </Accordion.Item>
-
-        <Accordion.Item eventKey="6">
-          <Accordion.Header>Undefined</Accordion.Header>
-          <Accordion.Body>
-            <p>Indicates that a variable has not been assigned a value.</p>
-            <pre className={styles.codeSnippet}>
-              <code>{`let und;`}</code>
-            </pre>
-          </Accordion.Body>
-        </Accordion.Item>
+        {data.getConceptsByCategory.map((concept, index) => (
+          <Accordion.Item eventKey={String(index)} key={index}>
+            <Accordion.Header>{concept.term}</Accordion.Header>
+            <Accordion.Body>
+              <p>{concept.description}</p>
+              <pre className={styles.codeSnippet}>
+                <code>{concept.code}</code>
+              </pre>
+            </Accordion.Body>
+          </Accordion.Item>
+        ))}
       </Accordion>
     </div>
   );

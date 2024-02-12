7d69a04aca4c1f8761c28b05e05f4c0c6d50c64e
Kandekore <darren@kandekore.net>
2024-02-12 22:58:50 +0000

    refcator server.js

--- server/server.js
+++ server/server.js
@@ -9,47 +9,33 @@ const path = require('path');
 const app = express();
 app.use(cors());
 
-// mongoose
-//   .connect("mongodb://127.0.0.1/jsConceptsDB", {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true
-//   })
-//   .then(() => console.log("Connected to MongoDB..."))
-//   .catch((err) => console.error("Could not connect to MongoDB...", err));
-
 // Connect to MongoDB
 mongoose
-  .connect(process.env.MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  })
+  .connect(process.env.MONGODB_URI)
   .then(() => console.log("Connected to MongoDB..."))
   .catch((err) => console.error("Could not connect to MongoDB...", err));
-
-// Serve static files from the React app build directory
-app.use(express.static(path.join(__dirname, '../build')));
-
-// Catchall handler to serve React's index.html
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../build/index.html'));
-});
-
-
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  playground: true 
 });
 
 async function startApolloServer() {
   await server.start();
   server.applyMiddleware({ app });
+
+ 
+  app.use(express.static(path.join(__dirname, '../client/build')));
+
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+  });
 }
 
 startApolloServer();
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
-  console.log(`Server listening at http://localhost:${PORT}/graphql`);
+  console.log(`Server listening at http://localhost:${PORT}${server.graphqlPath}`);
 });

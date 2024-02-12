9c94952643d73dee9a9598efbc8ef023494aae2e
Kandekore <darren@kandekore.net>
2024-02-12 22:20:07 +0000

    process.env

--- server/server.js
+++ server/server.js
@@ -5,13 +5,30 @@ const typeDefs = require("./schemas/typeDefs");
 const resolvers = require("./schemas/resolvers");
 const cors = require("cors");
 
+// mongoose
+//   .connect("mongodb://127.0.0.1/jsConceptsDB", {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true
+//   })
+//   .then(() => console.log("Connected to MongoDB..."))
+//   .catch((err) => console.error("Could not connect to MongoDB...", err));
+
+// Connect to MongoDB
 mongoose
-  .connect("mongodb://127.0.0.1/jsConceptsDB", {
+  .connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true
   })
   .then(() => console.log("Connected to MongoDB..."))
   .catch((err) => console.error("Could not connect to MongoDB...", err));
+
+// Serve static files from the React app build directory
+app.use(express.static(path.join(__dirname, '../build')));
+
+// Catchall handler to serve React's index.html
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../build/index.html'));
+});
 
 const app = express();
 app.use(cors());

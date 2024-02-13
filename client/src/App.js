1c1e477f0e2a15ac523d32c20f9957c96767fb17
Kandekore <darren@kandekore.net>
2024-02-13 13:48:34 +0000

    .env

--- client/src/App.js
+++ client/src/App.js
@@ -41,7 +41,7 @@ import { Helmet } from "react-helmet";
 
 // Initialize Apollo Client
 const client = new ApolloClient({
-  uri: "https://jsconcepts-619c289c467a.herokuapp.com/graphql",
+  uri: process.env.REACT_APP_GRAPHQL_URI || "http://localhost/graphql",
   cache: new InMemoryCache()
 });
 

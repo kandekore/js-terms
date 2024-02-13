6947a40ebe9a86b4734a8223f6fad0108a250308
Kandekore <darren@kandekore.net>
2024-02-13 10:04:33 +0000

    categories line 25

--- client/src/components/concepts/Variables.js
+++ client/src/components/concepts/Variables.js
@@ -22,7 +22,7 @@ const GET_CATEGORY_BY_NAME = gql`
 
 function VariablesPage() {
   const { loading, error, data } = useQuery(GET_CATEGORY_BY_NAME, {
-    variables: { name: "Variables" }
+    categories: { name: "Variables" }
   });
   console.log(data);  
   if (loading) return <p>Loading...</p>;
